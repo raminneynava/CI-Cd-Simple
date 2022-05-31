fdb229586c550463f4a10e2c681428dea90dd108
raminneynava <54467123+raminneynava@users.noreply.github.com>
2022-05-31 10:45:29 +0430

    initial commit

---
+++ UiTest/SampleTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UiTest
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void Just_For_Run_Sample_Test()
+        {
+            var myvar = "test";
+            bool result = (myvar == "test");
+            Assert.True(result);
+        }
+    }
+}
